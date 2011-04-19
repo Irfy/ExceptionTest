a8d1c286907d06a529234cf2d84ae131161dc033
Zvizdic Edin <Edin Zvizdic@.(none)>
2011-04-19 23:20:04 +0200

    Introduced different handling strategies via enums.

--- src/ExceptionTest.java
+++ src/ExceptionTest.java
@@ -6,59 +6,73 @@ import java.io.PrintStream;
 public class ExceptionTest {
 
 	public long maxLevel = 20;
-	private static final boolean DO_PRINT = true;
 
-	private static final ByteArrayOutputStream baos = new ByteArrayOutputStream(
-			1000000);
-	private static final PrintStream baps = new PrintStream(baos);
+	enum ExceptionHandler {
+		NONE {
+			void handle(Throwable t) {
+			}
+		},
+		PRINT_STDOUT {
+			void handle(Throwable t) {
+				t.printStackTrace(System.out);
+			}
+		},
+		PRINT_NULL {
+			private final OutputStream nullos = new OutputStream() {
+				@Override
+				public void write(int b) throws IOException {
+				}
 
-	private static final OutputStream nullos = new OutputStream() {
-		@Override
-		public void write(int b) throws IOException {
-		}
+				@Override
+				public void write(byte[] b) throws IOException {
+				}
 
-		@Override
-		public void write(byte[] b) throws IOException {
-		}
+				@Override
+				public void write(byte[] b, int off, int len) throws IOException {
+				};
+			};
+			private final PrintStream nullps = new PrintStream(nullos);
 
-		@Override
-		public void write(byte[] b, int off, int len) throws IOException {
+			void handle(Throwable t) {
+				t.printStackTrace(nullps);
+			}
+		},
+		PRINT_ARRAY {
+			private final ByteArrayOutputStream baos = new ByteArrayOutputStream(
+					1000000);
+			private final PrintStream baps = new PrintStream(baos);
+
+			void handle(Throwable t) {
+				t.printStackTrace(baps);
+			}
 		};
-	};
-	private static final PrintStream nullps = new PrintStream(nullos);
+		abstract void handle(Throwable t);
+	}
 
 	public static void main(String... args) {
 		ExceptionTest test = new ExceptionTest();
 
-		long start = System.currentTimeMillis();
-		int count = 10000;
-		for (int i = 0; i < count; i++)
-			try {
-				test.doTest(2, 0);
-			} catch (Exception ex) {
-				if (DO_PRINT) {
-					ex.printStackTrace(nullps);
-					baos.reset();
-				} else {
-					ex.getStackTrace();
+		for (ExceptionHandler eh : ExceptionHandler.values()) {
+			long start = System.currentTimeMillis();
+			int count = 10000;
+			for (int i = 0; i < count; i++)
+				try {
+					test.doTest(2, 0, eh);
+				} catch (Exception ex) {
+					eh.handle(ex);
 				}
-			}
-		long diff = System.currentTimeMillis() - start;
-		System.out.println(String.format("Average time for invocation: %1$.5f",
-				(double) diff / count));
+			long diff = System.currentTimeMillis() - start;
+			System.err.println(String.format("Average time for invocation: %1$.5f",
+					(double) diff / count));
+		}
 	}
 
-	public void doTest(int i, int level) {
+	public void doTest(int i, int level, ExceptionHandler eh) {
 		if (level < maxLevel)
 			try {
-				doTest(i, ++level);
+				doTest(i, ++level, eh);
 			} catch (Exception ex) {
-				if (DO_PRINT) {
-					ex.printStackTrace(nullps);
-					baos.reset();
-				} else {
-					ex.getStackTrace();
-				}
+				eh.handle(ex);
 				throw new RuntimeException("UUUPS", ex);
 			}
 		else if (i > 1)
